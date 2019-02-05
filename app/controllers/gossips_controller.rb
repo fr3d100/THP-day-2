4aac682c8cd801d5430ae7eddf7269a34bcd66f2
fr3d100 <f.bonnand@gmail.com>
2019-02-05 16:34:51 +0100

    New potin feature on

--- app/controllers/gossips_controller.rb
+++ app/controllers/gossips_controller.rb
@@ -1,13 +1,26 @@
 class GossipsController < ApplicationController
 
 	 def index	
-  	@gossips = Gossip.all
+  	@gossips = Gossip.all.reverse_order
   	puts Gossip.all
   end
    
    def show
   	@gossip = Gossip.find(params['id'])
   	puts @gossip
+  end
+
+  def new
+  	
+  end
+
+  def create
+  	g = Gossip.new
+  	g.title = params['gossip_title']
+  	g.content = params['gossip_content']
+  	g.user = User.first
+  	g.save
+  	redirect_to gossips_path
   end
 
 end
